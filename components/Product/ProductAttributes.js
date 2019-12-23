b7b33e1c5da1e4b8077beed53a52d8d38a34f147
Cem Ergin <birazgecemuzigi@gmail.com>
2019-12-22 21:54:43 -0500

    Section #5

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,5 +1,16 @@
-function ProductAttributes() {
-  return <>ProductAttributes</>;
+import { Header, Button } from 'semantic-ui-react'
+import ProductSummary from './ProductSummary'
+
+function ProductAttributes({ description }) {
+  return <>
+  <Header as="h3">About this product</Header>
+  <p>{description}</p>
+  <Button
+    icon="trasj alternate outline"
+    color="red"
+    content="Delete Product" 
+  />
+  </>;
 }
 
 export default ProductAttributes;
