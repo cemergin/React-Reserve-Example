6dc0a3dd5a6cdbf955c72ebbdbb170e6450e3562
Cem Ergin <birazgecemuzigi@gmail.com>
2020-01-07 15:09:18 -0500

    Section #10.1 #10.2

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,25 @@
 import { Button, Segment, Divider } from "semantic-ui-react";
+import React from "react";
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+
+  React.useEffect(() => {
+    setCartEmpty(products.length === 0);
+  }, [products]);
+
   return (
     <>
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> $0.00
-        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+        <Button
+          icon="cart"
+          color="teal"
+          disabled={isCartEmpty}
+          floated="right"
+          content="Checkout"
+        />
       </Segment>
     </>
   );
--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,12 +1,11 @@
-import { Item, ItemContent, Label } from 'semantic-ui-react'
-import AddProductToCart from './AddProductToCart'
+import { Item, ItemContent, Label } from "semantic-ui-react";
+import AddProductToCart from "./AddProductToCart";
 
-
-function ProductSummary({name, mediaUrl, _id, price, sku }) {
+function ProductSummary({ name, mediaUrl, _id, price, sku, user }) {
   return (
     <Item.Group>
       <Item>
-        <Item.Image size="medium" src={mediaUrl}/>
+        <Item.Image size="medium" src={mediaUrl} />
         <ItemContent>
           <Item.Header>{name}</Item.Header>
           <Item.Description>
@@ -14,12 +13,12 @@ function ProductSummary({name, mediaUrl, _id, price, sku }) {
             <Label>SKU : {sku}</Label>
           </Item.Description>
           <Item.Extra>
-            <AddProductToCart productId={_id}/>
+            <AddProductToCart user={user} productId={_id} />
           </Item.Extra>
         </ItemContent>
       </Item>
     </Item.Group>
-  )
+  );
 }
 
 export default ProductSummary;
