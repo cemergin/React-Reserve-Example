6e4cb4c07cc1912ebd95db1280fd3d328a6f9292
Cem Ergin <birazgecemuzigi@gmail.com>
2020-01-15 11:35:22 -0500

    baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://react-reserve-demo.now.sh"
+    ? "https://react-reserve-demo.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
